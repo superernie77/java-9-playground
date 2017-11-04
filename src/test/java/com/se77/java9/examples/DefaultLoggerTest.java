2c5a65dd4904a9ba8de3f3dfb7eabc038f57cc9f
superernie77 <superernie77@gmail.com>
2017-11-04 22:03:40 +0100

    new system logger test

---
+++ src/test/java/com/se77/java9/examples/DefaultLoggerTest.java
@@ -0,0 +1,28 @@
+package com.se77.java9.examples;
+
+import org.junit.Test;
+
+
+public class DefaultLoggerTest {
+
+    @Test
+    public void useLogger(){
+
+        System.Logger LOG = System.getLogger("com.se77.demologger");
+
+        // by default INFO log-level is used
+        boolean debugEnabled = LOG.isLoggable(System.Logger.Level.DEBUG);
+        LOG.log(System.Logger.Level.INFO, "Debug enabled: "+debugEnabled);
+
+        LOG.log(System.Logger.Level.DEBUG,"my debug log message");
+
+        LOG.log(System.Logger.Level.ERROR,"my error log message");
+
+        LOG.log(System.Logger.Level.INFO,"my info log message");
+
+        System.LoggerFinder finder = System.LoggerFinder.getLoggerFinder();
+
+    }
+}
+
+
