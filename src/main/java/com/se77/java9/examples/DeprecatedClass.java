7753753b17ccec7e5dc85b7c03e3382e03fde0e6
superernie77 <superernie77@gmail.com>
2017-11-05 12:31:18 +0100

    new deprecated annotation methods

---
+++ src/main/java/com/se77/java9/examples/DeprecatedClass.java
@@ -0,0 +1,6 @@
+package com.se77.java9.examples;
+
+// Example of the two new attributes of the @Deprecated annotation.
+@Deprecated(forRemoval = true, since = "8")
+public class DeprecatedClass {
+}
