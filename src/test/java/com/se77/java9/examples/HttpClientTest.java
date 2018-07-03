9b32b2a983d269ac70ec2c877a6e5710eda0e147
superernie77 <superernie77@gmail.com>
2018-07-03 21:08:33 +0200

    removed incubator test

--- src/test/java/com/se77/java9/examples/HttpClientTest.java
+++ src/test/java/com/se77/java9/examples/HttpClientTest.java
@@ -1,22 +1,22 @@
 package com.se77.java9.examples;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//import org.junit.jupiter.api.Disabled;
+//import org.junit.jupiter.api.Test;
 
 public class HttpClientTest {
 
     // ATTENTION!!! To run this test you need to add the HttpClient mudule to the JVM on startup.
     // This is what needs to be added to the java command
 	// --add-modules jdk.incubator.hhtpclient
-    @Test
-    @Disabled
-    public void getGooglePage() throws Exception {
-        HttpClientWrapper myWrapper = new HttpClientWrapper();
-
-        String result = myWrapper.getContent("http://www.google.de");
-
-        assertTrue(result != null);
-    }
+//    @Test
+//    @Disabled
+//    public void getGooglePage() throws Exception {
+//        HttpClientWrapper myWrapper = new HttpClientWrapper();
+//
+//        String result = myWrapper.getContent("http://www.google.de");
+//
+//        assertTrue(result != null);
+//    }
 }
